77eb771ad480f2cfe57d454f05139c4ac05040fd
Digital Site <113808139+sitetag@users.noreply.github.com>
2022-09-25 14:47:29 -0300

    1

---
+++ menu.js
@@ -0,0 +1,7 @@
+let show = true
+const menuSection = document.querySelector(".menu-section")
+const menuBurguer = menuSection.querySelector(".menu-burguer")
+menuBurguer.addEventListener("click", () =>{
+    menuSection.classList.toggle("on", show)
+    show = !show
+})
